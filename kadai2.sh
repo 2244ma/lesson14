d953126095194777ddbd0691ae70cf580e6cfcb0
email <takano2@aiit.ac.jp>
2017-06-11 12:08:03 +0900

    kadai2.sh

--- kadai2.sh
+++ kadai2.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 echo 'kadai2/17745118'
+if [ $# -ne  2 ]
+then
+  echo "errpr" 1>&2
+  exit 1
+fi
 
+a=$1
+b=$2
 
-a=48
-b=24
-
-while [ $b -eq 0 ]
+while [ $b -ne  0 ]
 do
- if [ $a -gt $b ]
- then
-   c=$a; a=$b; b=$c
-  fi
-  b==`expr $b % $b`
+ 
+   tmp=$a
+   a=$b
+   b=$tmp
+  
+  b=`expr $b % $a`
 done
