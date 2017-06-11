215d0bfe70bed7b99ad64c5ae4d3b11e68be81d1
email <takano2@aiit.ac.jp>
2017-06-11 23:14:08 +0900

    change file 2

--- same3.sh
+++ same3.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+# arguments check
+if [ $# -lt 1 ]; then
+  echo "kadai3.sh requires 2 string input" 1>&2
+  exit 1
+fi
 
+if [ $1 = $2 ]; then
+  echo "$1 $2 OK"
+else
+  echo "$1 $2 NG"
+fi
 
