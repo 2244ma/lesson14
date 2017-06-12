a3a3ba777d334873551922e6f456ea208a28c6a8
email <takano2@aiit.ac.jp>
2017-06-13 01:44:54 +0900

    0613-4

--- test2_kadai3.sh
+++ test2_kadai3.sh
@@ -16,15 +16,22 @@ echo "error" > $tmp-ans
 echo "TEST1 sucess"
 
 
-echo "Don't input > $tmp-ans
+echo "Don't input 0" > $tmp-ans
 ./kadai3.sh 0 10 > $tmp-err || ERROR_EXIT "exe error"
 diff $tmp-ans $tmp-err || ERROR_EXIT "0 correct?"
 
 echo "TEST2 sucess"
 
 
-echo " 2 > $tmp-ans
-./kadai3.sh 2 10 > $tmp-err || ERROR_EXIT "exe error"
-diff $tmp-ans $tmp-err || ERROR_EXIT "0 correct?"
+echo " 2" > $tmp-ans
+./kadai3.sh 2 10 > $tmp-err || ERROR_EXIT "正解です"
+diff $tmp-ans $tmp-err || ERROR_EXIT "error"
 
 echo "TEST3 sucess"
+
+
+echo "takano" > $tmp-ans
+./kadai3.sh 1 1 2 3 > $tmp-err || ERROR_EXIT "exe error"
+diff $tmp-ans $tmp-err || ERROR_EXIT "成功しました"
+
+echo "TEST4 sucess"
